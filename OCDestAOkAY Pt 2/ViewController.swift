1e239e4527c38473088e73bbead0952b808866ef
Hydrochlorick <hydrochlorick@icloud.com>
2021-03-16 21:37:14 -0700

    Initial Commit

---
+++ OCDestAOkAY Pt 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OCDestAOkAY Pt 2
+//
+//  Created by Rick Jacobson on 3/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
